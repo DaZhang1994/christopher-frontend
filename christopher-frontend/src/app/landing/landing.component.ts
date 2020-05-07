61393bbfa22334d409db567bcf123e2d18b84dec
DaZhang1994 <dazhang1994@gmail.com>
2020-05-06 22:22:39 -0700

    flipping cards

--- christopher-frontend/src/app/landing/landing.component.ts
+++ christopher-frontend/src/app/landing/landing.component.ts
@@ -1,12 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, HostListener, OnInit, QueryList, Renderer2, ViewChildren } from '@angular/core';
+import { CardRotatingComponent } from 'ng-uikit-pro-standard';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss'],
 })
-export class LandingComponent implements OnInit {
-  constructor() {}
+export class LandingComponent implements OnInit, AfterViewChecked {
 
-  ngOnInit(): void {}
+  cardWrappers: any;
+  cardImage: any;
+
+  @ViewChildren(CardRotatingComponent)
+  cards: QueryList<CardRotatingComponent>;
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+
+  }
+
+  ngOnInit(): void {
+    this.cardWrappers = this.el.nativeElement.querySelectorAll('.card-wrapper');
+    this.cardImage = this.el.nativeElement.querySelector('.img-fluid');
+  }
+
+  ngAfterViewChecked(): void {
+    this.updateCardsHeight();
+    this.resetCards();
+  }
+
+  updateCardsHeight() {
+    this.cardWrappers.forEach((cardWrapper: any) => {
+      this.renderer.setStyle(cardWrapper, 'height', this.cardImage.clientHeight + 'px')
+    });
+  }
+
+  resetCards() {
+    if(this.cards){
+      this.cards.forEach((card: any) => {
+        if(card.rotate && !card._cdRef._cdRefInjectingView[0].matches(':hover')) {
+          card.toggle();
+        }
+        if(!card.rotate && card._cdRef._cdRefInjectingView[0].matches(':hover')) {
+          card.toggle();
+        }
+      });
+    }
+  }
+
+  @HostListener('window:mousemove')
+  onMouseMove() {
+    this.resetCards();
+  }
+
 }
