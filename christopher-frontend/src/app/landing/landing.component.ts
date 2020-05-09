36060916e032caac179111260219db842a0dd998
DaZhang1994 <dazhang1994@gmail.com>
2020-05-08 20:40:29 -0700

    flipping cards optimization

--- christopher-frontend/src/app/landing/landing.component.ts
+++ christopher-frontend/src/app/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewChecked, Component, ElementRef, HostListener, OnInit, QueryList, Renderer2, ViewChildren } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { CardRotatingComponent } from 'ng-uikit-pro-standard';
 
 @Component({
@@ -6,15 +6,16 @@ import { CardRotatingComponent } from 'ng-uikit-pro-standard';
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss'],
 })
-export class LandingComponent implements OnInit, AfterViewChecked {
+export class LandingComponent implements OnInit {
 
   cardWrappers: any;
   cardImage: any;
+  currHoveredElement: any;
 
   @ViewChildren(CardRotatingComponent)
   cards: QueryList<CardRotatingComponent>;
 
-  constructor(private el: ElementRef, private renderer: Renderer2) {
+  constructor(private el: ElementRef) {
 
   }
 
@@ -23,33 +24,24 @@ export class LandingComponent implements OnInit, AfterViewChecked {
     this.cardImage = this.el.nativeElement.querySelector('.img-fluid');
   }
 
-  ngAfterViewChecked(): void {
-    this.updateCardsHeight();
-    this.resetCards();
-  }
-
-  updateCardsHeight() {
-    this.cardWrappers.forEach((cardWrapper: any) => {
-      this.renderer.setStyle(cardWrapper, 'height', this.cardImage.clientHeight + 'px')
-    });
-  }
-
-  resetCards() {
+  toggleCards() {
     if(this.cards){
       this.cards.forEach((card: any) => {
         if(card.rotate && !card._cdRef._cdRefInjectingView[0].matches(':hover')) {
           card.toggle();
         }
-        if(!card.rotate && card._cdRef._cdRefInjectingView[0].matches(':hover')) {
+        else if(!card.rotate && card._cdRef._cdRefInjectingView[0].matches(':hover')) {
           card.toggle();
         }
       });
     }
   }
 
-  @HostListener('window:mousemove')
-  onMouseMove() {
-    this.resetCards();
+  checkAndToggleCards(target: any) {
+    if(this.currHoveredElement != target) {
+      this.currHoveredElement = target;
+      this.toggleCards();
+    }
   }
 
 }
