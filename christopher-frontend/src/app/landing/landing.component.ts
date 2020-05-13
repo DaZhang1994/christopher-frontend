62773e994ba59483678c43b18124106288947da6
DaZhang1994 <dazhang1994@gmail.com>
2020-05-12 17:55:40 -0700

    people section

--- christopher-frontend/src/app/landing/landing.component.ts
+++ christopher-frontend/src/app/landing/landing.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { CardRotatingComponent } from 'ng-uikit-pro-standard';
 
 @Component({
   selector: 'app-landing',
@@ -74,23 +75,68 @@ export class LandingComponent implements OnInit {
     return R;
   }
 
-  @ViewChild('like')
-  like: any;
+  @ViewChild('follow')
+  follow: ElementRef;
 
   @ViewChild('next')
-  next: any;
+  next: ElementRef;
+
+  @ViewChild('peopleCard1')
+  peopleCard1: CardRotatingComponent;
+
+  @ViewChild('peopleCardWrapper1')
+  peopleCardWrapper1: ElementRef;
 
   startedDrag: boolean = false;
+  hoverOnFollow: boolean = false;
+  hoverOnNext: boolean = false;
 
   ngOnInit(): void {
     this.postSlides = this.chunk(this.postCards, 3);
   }
 
-  onDrag(_event: any) {
-    this.startedDrag = true;
+  onRightClickCard() {
+    this.peopleCard1.toggle();
+    return false;
   }
 
-  endDrag(_$event: any) {
+  onDrag(_event: any) {
+    this.startedDrag = true;
+    this.hoverOnFollow = false;
+    this.hoverOnNext = false;
+  }
+
+  onEndDrag(event: any) {
     this.startedDrag = false;
+    this.hoverOnFollow = false;
+    this.hoverOnNext = false;
+    event.source._dragRef.reset()
+  }
+
+  onDragMove(_$event: any) {
+    if(this.collide(this.peopleCardWrapper1, this.follow)) {
+      this.hoverOnFollow = true;
+      this.hoverOnNext = false;
+    }
+    else if(this.collide(this.peopleCardWrapper1, this.next)) {
+      this.hoverOnNext = true;
+      this.hoverOnFollow = false;
+    }
+    else {
+      this.hoverOnFollow = false;
+      this.hoverOnNext = false;
+    }
+  }
+
+  collide(el1: ElementRef, el2: ElementRef) {
+    const rect1 = el1.nativeElement.getBoundingClientRect();
+    const rect2 = el2.nativeElement.getBoundingClientRect();
+
+    return !(
+      rect1.top > rect2.bottom ||
+      rect1.right < rect2.left ||
+      rect1.bottom < rect2.top ||
+      rect1.left > rect2.right
+    );
   }
 }
